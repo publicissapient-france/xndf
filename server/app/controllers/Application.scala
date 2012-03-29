08c1e6bfa9f2e3a9f9420bc27007bcfbe979e0ef
Jean Helou <jean.helou@gmail.com>
2012-03-29 23:57:21 +0200

    openid in progress

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -3,17 +3,116 @@ package controllers
 import play.api._
 import play.api.libs.json.Json._
 import play.api.mvc._
-import models.User
+import play.api.data._
+import play.api.data.Forms._
+import models._
+import views._
+import play.api.libs.openid._
+import play.api.libs.concurrent.Thrown
+import play.api.libs.concurrent.Redeemed
 
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+object Application extends Controller with Secured {
+
+  def index = IsAuthenticated { user => implicit request =>
+     Ok("")
+    //Ok(html.index(user))
   }
 
-  def user(id:Long) = Action { request =>
-    val user = User.findById(id)
-    user.map( user => Ok(toJson(user.toJson()))).getOrElse(BadRequest("perdu"))
+  val loginForm = Form(
+    "user" -> text)
+
+  def login = Action { implicit request =>
+    Ok(html.login(loginForm))
+  }
+
+  def authenticate = Action { implicit request =>
+    Form(single(
+      "openid" -> nonEmptyText)).bindFromRequest.fold(
+      error => {
+        Logger.info("bad request " + error.toString)
+        BadRequest(error.toString)
+      },
+      {
+        case (openid) => AsyncResult(
+            OpenID.redirectURL(
+                openid, 
+                routes.Application.openIDCallback().absoluteURL(),
+                Seq("email" -> "http://schema.openid.net/contact/email")
+                )
+          .extend(_.value match {
+            case Redeemed(url) => Redirect(url)
+            case Thrown(t) => Redirect(routes.Application.login())
+          }))
+      })
+  }
+
+  def openIDCallback = Action { implicit request =>    
+	  AsyncResult(
+	    OpenID.verifiedId.extend( _.value match {
+	      case Redeemed(info) => doAuthenticate(info)
+	      case Thrown(t) => {
+	        // Here you should look at the error, and give feedback to the user
+	        Redirect(routes.Application.login)
+	      }
+	    })
+  )
   }
   
+  private def doAuthenticate(info:UserInfo) = {
+    val user = for{ 
+      email <- info.attributes.get("email")      
+    } yield User.authenticate("name",email, info.id)
+    user.map{ u => 
+      			Ok(u.id + "\n" + info.attributes).withSession("verifiedId"->u.verifiedId) }
+    	.getOrElse(Redirect(routes.Application.login))
+  }
+
+  /**
+   * Logout and clean the session.
+   */
+  def logout = Action {
+    Redirect(routes.Application.login).withNewSession.flashing(
+      "success" -> "You've been logged out")
+  }
+
+}
+
+/**
+ * Provide security features
+ */
+trait Secured {
+
+  /**
+   * Retrieve the connected user email.
+   */
+  private def username(request: RequestHeader) = request.session.get("verifiedId")
+
+  /**
+   * Redirect to login if the user in not authorized.
+   */
+  private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
+
+  // ---
+
+  /**
+   * Action for authenticated users.
+   */
+  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized) { user =>
+    Action(request => f(user)(request))
+  }
+
+  /**
+   * Check if the connected user is a owner of this task.
+   *
+  
+  def IsOwnerOf(task: Long)(f: => String => Request[AnyContent] => Result) = IsAuthenticated { user =>
+    request =>
+      if (Task.isOwner(task, user)) {
+        f(user)(request)
+      } else {
+        Results.Forbidden
+      }
+  }*/
+
 }
--- server/test/models/UserSpec.scala
+++ server/test/models/UserSpec.scala
@@ -10,29 +10,62 @@ import anorm._
 import anorm.SqlParser._
 import play.api.Logger
 
-@RunWith(classOf[JUnitRunner])
 class UserSpec extends Specification {
 
   import models.User
 
   "User model" should {
-    "must save a user" in {
-
+    "save a user" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
-        user1.save() === 1001 and
-        User.count() === 2
+        user1.save() === 1000 and
+        User.count() === 1
       }
     }
+
     "be retrieved by id" in {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        user1.save()
+        user2.save()
         val Some(user)=User.findById(1000)
-        user.firstName must equalTo("nadia")
+        user.name must equalTo(user1.name)
       }
     }
+
+    "be retrieved by verifiedId" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        user1.save()
+        user2.save()
+        val Some(user)=User.findByVerifiedId("id2")
+        user.name must equalTo(user2.name)
+      }
+    }
+
     "be converted from and to json" in {
-      User.fromJson(user2.toJson()) === Right(user2)
+      User.fromJson(user2.toJson(), "id2") === Right(user2)
+    }
+
+    "be created and saved in the database" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val u = User.create("Josiane", "email3", "id3")
+        User.count() === 1 and u.name === User.findByVerifiedId("id3").get.name
+      }
+    }
+
+    "be authenticated when user exists" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val u = User.create("Josiane", "email3", "id3")
+        val authenticated=User.authenticate("Josiane", "email3", "id3")
+        User.count() === 1 and u === authenticated
+      }
+    }
+    "be authenticated when user does not exist" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        val authenticated=User.authenticate("Josiane", "email3", "id3")
+        User.count() === 1 and authenticated.id === Id(1000)
+      }
     }
   }
-  private val user1 = User(NotAssigned, "firstname", "lastname", "email")
-  private val user2 = User(Id(1), "firstname", "lastname", "email")
+
+  private val user1 = User(NotAssigned, "John Doe", "email1", "id1")
+  private val user2 = User(Id(1), "Jane Doe", "email2", "id2")
 }
