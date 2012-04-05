2e052d2f0780ad4267d6d1c545ff100269636c54
Pierre Laporte <pierre@pingtimeout.fr>
2012-04-05 16:08:30 +0200

    Replaced the JSON URL for getting a single user

--- server/public/javascripts/models/usermodel.js
+++ server/public/javascripts/models/usermodel.js
@@ -1,7 +1,8 @@
 //noinspection JSUnresolvedVariable
 App.models.User = Backbone.Model.extend({
-    urlRoot:"http://localhost:9000/users"
-})
+    urlRoot:"http://localhost:9000/user/"
+});
+
 App.models.UserCollection = Backbone.Collection.extend({
     model:App.models.User,
     url:"http://localhost:9000/users"
