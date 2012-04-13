fb6de0d6b0c2f508eb4474783172a9721a58fe0e
Jean Helou <jhelou@xebia.fr>
2012-04-13 16:51:43 +0200

    avancement sur implem  de la note de frais

--- server/app/models/domain.scala
+++ server/app/models/domain.scala
@@ -5,39 +5,68 @@ import anorm._
 import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
-
-/**
- * @author David Galichet.
- */
+import play.api.libs.json._
 
 package object domain {
-
+  def foo(e:Expense):String={
+    e match {
+      case Lodging => {"lodging"}
+      case Transportation =>{"Transportation"}
+    }
+  }
 }
 
-case class ExpenseReport(id:Pk[Long], number:String, from:Date, to:Date, userId:Id[Long]) {
+case class ExpenseReport(id:Pk[Long], from:Date, to:Date, userId:Id[Long], lines:Seq[ExpenseLine]) {
 
 }
 
 case class ExpenseLine(id:Pk[Long],
-                       expenseReportId:List[Id[Long]],
-                       creationDate:Date,
+                       expenseReport:ExpenseReport,
+                       valueDate:Date,
                        evidenceNumber:Int,
                        account:String,
                        description:String,
                        expense:Expense) {
 
+  implicit object ExpenseLineFormat extends Format[ExpenseLine] {
+
+    def reads(json: JsValue): ExpenseLine = {
+      ExpenseLine(NotAssigned,ExpenseReport(NotAssigned,new Date(), new Date(), Id(1l), Seq()),new Date(),1,"","",Lodging(1))
+    }
+
+    //unmarshaling to JSValue is covered in the next paragraph
+
+    def writes(line: ExpenseLine): JsValue = JsObject(
+      Seq("id" -> JsNumber(line.id.get))
+    )
+  }
 }
 
 
 sealed trait Expense {
   val amount:Double
+  val qualifier:String
 }
 
-case class Lodging(amount:Double) extends Expense
-case class Transportation(amount:Double) extends Expense
-case class Gas(amount:Double) extends Expense
-case class Meal(amount:Double) extends Expense
-case class Phone(amount:Double) extends Expense
-case class Internet(amount:Double) extends Expense
-case class Other(amount:Double) extends Expense
+case class Lodging(amount:Double) extends Expense{
+  val qualifier="Lodging"
+}
+case class Transportation(amount:Double) extends Expense{
+  val qualifier="Transportation"
+}
+case class Gas(amount:Double) extends Expense{
+  val qualifier="Gas"
+}
+case class Meal(amount:Double) extends Expense{
+  val qualifier="Meal"
+}
+case class Phone(amount:Double) extends Expense{
+  val qualifier="Phone"
+}
+case class Internet(amount:Double) extends Expense{
+  val qualifier="Internet"
+}
+case class Other(amount:Double) extends Expense{
+  val qualifier="Other"
+}
 
---
+++ server/test/models/ExpenseReportSpec.scala
@@ -0,0 +1,10 @@
+package models
+
+/**
+ * User: Jean Helou
+ * Date: 11/04/12
+ */
+
+class ExpenseReportSpec {
+
+}
