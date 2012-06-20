dba8bc5863e57a0fe8dd3f1469c602fcf914dc27
Jean Helou <jean.helou@gmail.com>
2012-06-20 23:48:28 +0200

    Fix add new expense line
    factorize template rendering code

--- server/public/javascripts/views/expense-details.js
+++ server/public/javascripts/views/expense-details.js
@@ -8,32 +8,37 @@ define([
     var ExpenseDetailsView = Backbone.View.extend({
         self:this,
         initialize:function () {
-            this.model.bind("reset", this.render, this);
+            this.model.on("reset", this.render, this);
         },
         events:{
             "click #put":"saveExpense",
             "click #add":"addLine"
         },
 
-        render:function (eventName) {
-            var $element=$(_.template(template, this.model.toJSON()));
-            _.each(this.model.get('lines'), function(line,index){
-                var selectedOption = $element.find('#expenseType').eq(index).children('option[value='+line.expenseType+']');
-                selectedOption.attr('selected',true);
+        renderTemplate:function (json) {
+            var $element = $(_.template(template, json));
+            _.each(json.lines, function (line, index) {
+                var selectedOption = $element.find('#expenseType').eq(index).children('option[value=' + line.expenseType + ']');
+                selectedOption.attr('selected', true);
             });
+            return $element;
+        },
+
+        render:function () {
+            var $element = this.renderTemplate(this.model.toJSON());
             this.setElement($element);
             $('#content').replaceWith($element);
         },
 
         saveExpense:function () {
             var elements = $('#content td#expense-line');
-            var view =this;
+            var view = this;
             var lines = _.map(elements, function (template) {
                 t = $(template);
                 return {
                     "expense":(+t.find('#amount').val()),
                     "description":t.find('#description').val(),
-                    "valueDate":moment(t.find('#valueDate').val(), window.dateFormat).format(moment.defaultFormat),
+                    "valueDate":t.find('#valueDate').val(),
                     "expenseType":t.find('#expenseType').val(),
                     "id":null,
                     "expenseReportId":null,
@@ -45,22 +50,17 @@ define([
                 this.model.unset('id')
             }
             this.model.save({
-                startDate:moment($('#startDate').val(), window.dateFormat).format(),
-                endDate:moment($('#endDate').val(), window.dateFormat).format(),
+                startDate:$('#startDate').val(),
+                endDate:$('#endDate').val(),
                 total:$('#total').val(),
                 lines:lines
             });
             return false;
         },
-        addLine:function(){
-             var table=$("#content table");
-            _.each( this.model.defaults.lines, function (line) {
-                console.log(line);
-                line.valueDate = moment().format(window.dateFormat);
-                var el = $(_.template(lineTemplate, line));
-                el.find('select option[value=' + line.expenseType + ']').attr('selected', true)
-                table.append(el);
-            })
+        addLine:function () {
+            var $table = $("#content table");
+            var $element = this.renderTemplate(this.model.defaults);
+            $table.append($element.find('table tbody tr'));
         },
         close:function () {
             $(this.el).unbind();
