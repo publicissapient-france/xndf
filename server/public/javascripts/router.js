b3268d731349ee6ad6d2c1d42313dd61e50c1254
Jean Helou <jean.helou@gmail.com>
2012-06-20 23:03:07 +0200

    Put expense and homeview in Approuter's whole scope
    This way, we can get the model from the loaded collection instead of reloading from the server.

--- server/public/javascripts/router.js
+++ server/public/javascripts/router.js
@@ -1,7 +1,9 @@
 // Filename: router.js
 define([
-    'Backbone'
-], function(Backbone){
+    'Backbone',
+    'models/expense',
+    'views/home'
+], function(Backbone, Expense, homeview){
 
     var AppRouter = Backbone.Router.extend({
         routes: {
@@ -11,36 +13,26 @@ define([
             "expense/:id":"edit_expense",
             // Default
             "":"home",
-            "*other"    : "defaultAction"
+            "*other"    : "home"
         },
 
         home: function(){
-
-            if(this.homeview==undefined){
-                var self=this;
-                require(['views/home'], function(homeview){
-                    self.homeview=homeview;
-                });
-            }else{
-                this.homeview.refresh();
-            }
+            homeview.refresh();
         },
+
         edit_expense: function(id){
-            require([ 'models/expense', 'views/expense-details'],function(Expense, ExpenseDetailsView){
-                var expense = new Expense({id:id});
+            require(['views/expense-details'],function(ExpenseDetailsView){
+                var expense = homeview.model.get(id);
                 expense.fetch({success:function() {
                     new ExpenseDetailsView({model:expense}).render();
                 }})
             });
         },
+
         new_expense: function(id){
-            require([ 'models/expense', 'views/expense-details'],function(Expense, ExpenseDetailsView){
+            require(['views/expense-details'],function(ExpenseDetailsView){
                 new ExpenseDetailsView({model:new Expense()}).render();
             });
-        },
-        defaultAction: function(actions){
-            // We have no matching route, lets just log what the URL was
-            console.log('No route:', actions);
         }
     });
 
