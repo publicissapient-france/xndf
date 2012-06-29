f0651f5cdc618ff789d0687644269601ab0c684f
Jean Helou <jean.helou@gmail.com>
2012-06-29 03:17:17 +0200

    start a mongo server in dev mode, stop it when stopping the app

--- server/app/Global.scala
+++ server/app/Global.scala
@@ -1,11 +1,35 @@
-import models.User
-import anorm._
+import de.flapdoodle.embedmongo.config.MongodConfig
+import de.flapdoodle.embedmongo.distribution.Version
+import de.flapdoodle.embedmongo.{MongodProcess, MongodExecutable, MongoDBRuntime}
 import play.api.{Application, GlobalSettings}
 
+import play.api.Mode
 /**
  * @author David Galichet.
  */
 
 object Global extends GlobalSettings {
 
+  override def beforeStart(app: Application) {
+    app.mode match {
+      case Mode.Dev => startMongo(app)
+      case _ => Unit
+    }
+
+    super.beforeStart(app)
+  }
+
+  lazy val _mongodExe:MongodExecutable ={
+    val runtime:MongoDBRuntime = MongoDBRuntime.getDefaultInstance();
+    runtime.prepare(new MongodConfig(Version.V2_0, 27017,false));
+  }
+  lazy val _mongod:MongodProcess={_mongodExe.start()}
+
+  def startMongo(app:Application) {
+    _mongod
+  }
+  override def onStop(app: Application) {
+    _mongod.stop()
+    _mongodExe.cleanup()
+  }
 }
