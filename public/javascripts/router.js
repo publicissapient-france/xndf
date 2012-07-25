add9562c2e0396594f736217e7363ce5c51568d3
Jean Helou <jean.helou@gmail.com>
2012-07-26 00:47:17 +0200

    Router cleanup

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -8,11 +8,9 @@ define([
     var AppRouter = Backbone.Router.extend({
         routes: {
             // Define some URL routes
-            "home": "home",
             "expense/new":"new_expense",
             "expense/:id":"edit_expense",
             // Default
-            "":"home",
             "*other"    : "home"
         },
 
@@ -23,13 +21,11 @@ define([
         edit_expense: function(id){
             require(['views/expense-details'],function(ExpenseDetailsView){
                 var expense = homeview.model.get(id);
-                expense.fetch({success:function() {
-                    new ExpenseDetailsView({model:expense}).render();
-                }})
+                new ExpenseDetailsView({model:expense}).render();
             });
         },
 
-        new_expense: function(id){
+        new_expense: function(){
             require(['views/expense-details'],function(ExpenseDetailsView){
                 new ExpenseDetailsView({model:new Expense()}).render();
             });
