75fd5a3f02b75d669ab1ae1b93dd8826eaad3e53
Jean Helou <jean.helou@gmail.com>
2012-08-28 19:24:42 +0200

    Use MongoDB not MongoConnection for gridfs

--- app/libs/mongo/package.scala
+++ app/libs/mongo/package.scala
@@ -4,9 +4,14 @@ import play.api.{Play, Application, PlayException}
 import play.api.Play.current
 import se.radley.plugin.salat.SalatPlugin
 import com.mongodb.casbah.gridfs.GridFS
+import com.mongodb.casbah.{MongoDB, MongoConnection}
 
 package object mongo {
   def gridFS(collectionName: String, sourceName:String = "default")(implicit app: Application): GridFS = {
-    app.plugin[SalatPlugin].map(plugin => GridFS(plugin.source(sourceName).connection)).getOrElse(throw PlayException("SalatPlugin is not registered.", "You need to register the plugin with \"500:se.radley.plugin.salat.SalatPlugin\" in conf/play.plugins"))
+    app.plugin[SalatPlugin].map({ plugin =>
+      val connection: MongoConnection = plugin.source(sourceName).connection
+      val db: MongoDB = connection(plugin.source(sourceName).db)
+      GridFS(db)
+    }).getOrElse(throw PlayException("SalatPlugin is not registered.", "You need to register the plugin with \"500:se.radley.plugin.salat.SalatPlugin\" in conf/play.plugins"))
   }
 }
