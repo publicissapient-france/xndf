de58a76fcbb9006457e4d6021d18798ef7e0aeed
spark <spark@spark-VS.(none)>
2012-09-26 20:45:35 +0200

    Fixes issue #35 : add requirejs i18n plugin, and i18n translation module to be used in templates or views.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,13 +3,17 @@
 // There usage will become more apparent futher along in the tutorial.
 require.config({
     paths: {
+        text : "libs/require.text",
+        i18n : "libs/require.i18n",
+
         //jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery',
         jquery: 'libs/jquery',
         //jquery: 'libs/zepto', -- not working for now, for saveFile in expense-details
         hogan: 'http://twitter.github.com/hogan.js/builds/2.0.0/hogan-2.0.0.js',
         Underscore: 'libs/underscore/underscore-min',
         Backbone: 'libs/backbone/backbone',
-        Moment: 'libs/moment.min'
+        Moment: 'libs/moment.min',
+        translations : "i18n"
     },
 
     shim: {
@@ -27,7 +31,6 @@ require.config({
             //Once loaded, use the global 'Backbone' as the
             //module value.
             exports: 'Backbone'
-
         }
     }
 });
@@ -36,8 +39,9 @@ require([
     // Some plugins have to be loaded in order due to there non AMD compliance
     // Because these scripts are not "modules" they do not pass any values to the definition function below
     'jquery',
+    'translations',
     'app'
-], function($,App){
+], function($,translations,App){
     // The "app" dependency is passed in as "App"
     // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
     App.initialize();
